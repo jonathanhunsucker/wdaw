12417ffa16c035a014802ad01608fa88f4cbe72e
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2019-10-27 22:02:41 -0400

    Add factory method for creating Note from steps from middle A

--- src/music/Note.js
+++ src/music/Note.js
@@ -1,3 +1,25 @@
+function mathRange(low, high) {
+  let range = [];
+  let current = low;
+  while (current <= high) {
+    range.push(current);
+    current++;
+  }
+
+  return range;
+}
+
+function mathModulo(x, mod) {
+  let value = x % mod;
+  if (value < 0) {
+    value += mod;
+  }
+
+  return value;
+}
+
+const letters = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+
 export default class Note {
   /**
    * @param {string} pitch - In scientific pitch notation, eg. C2, Eb6, F#-1
@@ -11,6 +33,12 @@ export default class Note {
 
     this.parts = parts;
     this.pitch = pitch;
+  }
+  static fromStepsFromMiddleA(steps) {
+    const octaves = Math.floor(steps / 12) + 3;
+    const step = mathModulo(steps, 12);
+    const letter = letters[step];
+    return new Note(`${letter}${octaves}`);
   }
   static parse(object) {
     return new Note(object.pitch);
@@ -28,7 +56,7 @@ export default class Note {
   }
   get step() {
     const [wholeThing, note, accidental, octave] = this.parts;
-    const integerNote = ['A', '_', 'B', 'C', '_', 'D', '_', 'E', 'F', '_', 'G', '_'].indexOf(note);
+    const integerNote = letters.indexOf(note);
     const accidentalVariation = {'b': -1, '#': 1, '': 0}[accidental];
 
     return integerNote + accidentalVariation;
