3bf411e493debe853121c5a028281af7627e9980
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2019-11-21 16:19:27 -0700

    Support read only mode for Checkbox

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 
 
 const Checkbox = React.memo(function Checkbox(props) {
+  const { value, onChange, ...remainder } = props;
+
   const checkboxRef = useRef(null);
 
   const indeterminate = props.value === 'indeterminate';
@@ -11,30 +13,33 @@ const Checkbox = React.memo(function Checkbox(props) {
     checkboxRef.current.indeterminate = indeterminate;
   }, [indeterminate]);
 
-  const handleChange = (e) => {
-    const shiftWasPressed = e.nativeEvent.shiftKey;
-    const isChecked = e.target.checked;
 
-    const value = {
-      [[true, true].toString()]: 'indeterminate',
-      [['indeterminate', true].toString()]: false,
-      [[false, true].toString()]: 'indeterminate',
-      [[true, false].toString()]: false,
-      [['indeterminate', false].toString()]: false,
-      [[false, false].toString()]: true,
-    }[[props.value, shiftWasPressed].toString()];
+  const inputProps = {};
+  inputProps.type = 'checkbox';
+  inputProps.ref = checkboxRef;
+  inputProps.checked = checked;
 
-    props.onChange(value);
-  };
+  if (props.hasOwnProperty('onChange')) {
+    inputProps.onChange = (e) => {
+      const shiftWasPressed = e.nativeEvent.shiftKey;
+      const isChecked = e.target.checked;
 
-  return (
-    <input
-      ref={checkboxRef}
-      type="checkbox"
-      checked={checked}
-      onChange={handleChange}
-    />
-  );
+      const value = {
+        [[true, true].toString()]: 'indeterminate',
+        [['indeterminate', true].toString()]: false,
+        [[false, true].toString()]: 'indeterminate',
+        [[true, false].toString()]: false,
+        [['indeterminate', false].toString()]: false,
+        [[false, false].toString()]: true,
+      }[[props.value, shiftWasPressed].toString()];
+
+      props.onChange(value);
+    };
+  } else {
+    inputProps.readOnly = true;
+  }
+
+  return (<input {...inputProps}/>);
 });
 
 export default Checkbox;
